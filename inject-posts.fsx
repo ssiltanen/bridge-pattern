476f6bf943e2a1e8b4faaa18576b7b623147e020
Sampo Siltanen <sampo.siltanen@gmail.com>
2024-09-28 14:25:50 +0300

    Add metadata and styles to posts
    Metadata is added as frontmatter variables. Jekyll supports these out
    of the box and allows to configure jekyl and our langing page neatly in
    one place.
    
    During CI the inject script parses the metadata comments from posts
    to inject the properties to the posts on the timeline. This includes
    sorting them in a descending order using the date.
    
    Add styles for tags to wrap around when there are many tags.

--- inject-posts.fsx
+++ inject-posts.fsx
@@ -1,19 +1,67 @@
 open System
 
-let template file = $"""
-        <div class="timeline-item">
-            <a href="{file}">
-                <div class="content">
-                    <p class="commit-message">Some Blog Post</p>
-                    <p class="commit-date">2024-08-07</p>
-                    <span class="tag">v1.1</span>
-                </div>
-            </a>
-        </div>"""
+type Metadata = {
+    title: string
+    date: string
+    tags: string list
+}
+
+let template meta file = 
+    let tagSpans = 
+        match meta.tags with
+        | [] -> ""
+        | _ -> 
+            let spans = 
+                meta.tags 
+                |> List.map (fun tag -> $"""<span class="tag">{tag}</span>""") 
+                |> String.concat ""
+            $"""<div class="tags">{spans}</div>"""
+    
+    $"""
+<div class="timeline-item">
+    <a href="{file}">
+        <div class="content">
+            <p class="commit-message">{meta.title}</p>
+            <p class="commit-date">{meta.date}</p>
+            {tagSpans}
+        </div>
+    </a>
+</div>"""
+
+
+
+let getMetadata (file: string) =
+    let meta =
+        IO.File.ReadAllLines file
+        |> Array.skipWhile (fun line -> String.IsNullOrWhiteSpace line || line = "---")
+        |> Array.takeWhile ((<>) "---")
+        |> Array.choose (fun line ->
+            let split = line.Split ": "
+            (Array.tryHead split, Array.tryItem 1 split)
+            ||> Option.map2 (fun key value -> key, value)
+        )
+
+    let get key =
+        Array.tryFind (fst >> (=) key)
+        >> Option.map snd 
+        >> Option.defaultWith (fun () -> failwithf "File %s is missing meta key %s" file key)
+
+    let splitToList (str: string) = str.Split(" ", StringSplitOptions.RemoveEmptyEntries) |> Array.toList 
+
+    {
+        title = meta |> get "title"
+        date = meta |> get "date"
+        tags = meta |> get "tags" |> splitToList
+    }
+
+let replaceExtension (ext: string) (file: string) =
+    file.Split "." |> Array.head |> fun name -> $"{name}.{ext}"
 
 let content =
     System.IO.Directory.GetFiles "posts"
-    |> Array.map ((fun file -> file.Replace(".md", ".html")) >> template)
+    |> Array.map (fun file -> file, getMetadata file)
+    |> Array.sortByDescending (snd >> _.date)
+    |> Array.map (fun (file, meta) -> replaceExtension "html" file |> template meta)
     |> String.concat "\n"
 
 let index = System.IO.File.ReadAllText "index.html"
